dff40ead5c8add1f64e9417daeb4c63d16bdf54f
Stephen Sinclair <sinclair@music.mcgill.ca>
2013-09-04 11:20:26 +0200

    Ensure C ABI for libcrtaudio.

--- crtaudio/crtaudio.h
+++ crtaudio/crtaudio.h
@@ -3,8 +3,16 @@
 
 typedef void crtaudio;
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 crtaudio *crtaudio_new();
 void crtaudio_free(crtaudio *rta);
 void crtaudio_tick(crtaudio *rta, float sample);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
