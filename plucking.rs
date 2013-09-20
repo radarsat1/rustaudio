2411c8d773af799242a09842b28bb45f50d97333
Stephen Sinclair <sinclair@music.mcgill.ca>
2013-09-20 14:44:53 +0200

    Make the audio task SingleThreaded, remove call to deschedule.
    This makes it work on my single-core laptop.
    
    Audio task now also waits for the initial instruction before opening
    the soundcard and playing anything.

--- plucking.rs
+++ plucking.rs
@@ -6,7 +6,7 @@
 use std::num::sin;
 use std::rt::io::Timer;
 use std::io::println;
-use std::task::{spawn_with,deschedule};
+use std::task::{spawn_sched,SingleThreaded};
 use std::comm::{stream,Port,Chan};
 
 mod crtaudio;
@@ -17,15 +17,15 @@ fn main() {
     /* Audio is processed on a background thread, while must manually
      * yield after each sample. */
 
-    do spawn_with(in_port) |in_port| {
+    do spawn_sched(SingleThreaded) {
+        let mut i = in_port.recv();
         let rta = crtaudio::CRtAudio::new();
-        let mut i = 0;
         while (i != -1) {
             let mut sample = 0.0;
 
             if (i < 48000) {
                 let power = (std::int::max(10000-i, 0) as float)/10000.0 ;
-                sample = sin((i as float)/48000.0 * 2.0 * 3.14 * 220.0)
+                sample = sin((i as float)/48000.0 * 2.0 * 3.14 * 440.0)
                     * power * power;
                 i += 1;
             }
@@ -33,27 +33,27 @@ fn main() {
             if (in_port.peek()) {
                 match (in_port.try_recv()) {
                     Some(x) => { i = x }
-                    None => { i = -1 }
+                    None => break
                 }
             }
 
             rta.tick( sample );
-            deschedule();
         }
     }
 
     /* Reset the audio task's `i` variable, re-triggering a pluck
      * sound once a second. */
 
-    let mut k=1;
+    let mut k=0;
     while (k <= 3) {
+        do Timer::new().map_move |mut t| { t.sleep(1000) };
         println(fmt!("%d",k));
         out_port.send(0);
-        do Timer::new().map_move |mut t| { t.sleep(1000) };
         k += 1;
     }
 
     /* A value of -1 signals the end of the program to the audio task. */
 
+    do Timer::new().map_move |mut t| { t.sleep(1000) };
     out_port.send(-1);
 }
